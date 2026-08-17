9ef1e9ee5d7f7c856a3d453fc516eb4fcd5f84bb
Anuj Jakhar <anuj.jakhar.2104@gmail.com>
2026-08-17 16:50:34 +0530

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int n,m;
+    vector<vector<int>> dp;
+    int solve(int i , int j , string& s , string& t){
+        if (i >= n) return j >= m ? 1 : 0;
+        if (j >= m) return 1;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int take = 0;
+        if (s[i] == t[j]) take = solve(i+1 , j+1 , s , t);
+
+        int ntake = solve(i+1 , j , s , t);
+
+        return dp[i][j] = take + ntake;
+    }
+    int numDistinct(string s, string t) {
+        n = s.size();
+        m = t.size();
+        dp.resize(n , vector<int>(m , -1));
+
+        return solve(0 , 0, s, t);
+    }
+};
